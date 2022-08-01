ebef20b9d536997e97bd251b7a903872bdcc04a2
Abhay0294 <abhaypratap349@gmail.com>
2022-08-01 11:35:29 +0530

    1 file modified

--- Practice-Components/Rest-Parameters/rest-parameters.js
+++ Practice-Components/Rest-Parameters/rest-parameters.js
@@ -1,11 +1,6 @@
 function LinkedIn(param1, param2, ...restPararams) {
   var restParameters = restPararams;
-  // let params = (restPararams) => {
-  //   for (let i = 0; i < restPararams.length; i++) {
-  //     restParameters.push(restPararams[i]);
-  //   }
-  // };
-  // params(restPararams);
+
   console.log(restParameters);
   console.log(param1);
   console.log(param2);
