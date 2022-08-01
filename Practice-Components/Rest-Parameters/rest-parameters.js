8a09cf8ccece82cabbeac8b54fca8745d4da5383
Abhay0294 <abhaypratap349@gmail.com>
2022-08-01 11:27:47 +0530

    1 file modified, 1 added

---
+++ Practice-Components/Rest-Parameters/rest-parameters.js
@@ -0,0 +1,14 @@
+function LinkedIn(param1, param2, ...restPararams) {
+  console.log(param1);
+  console.log(param2);
+  console.log(restPararams.Description);
+}
+
+LinkedIn(
+  'Name',
+  'Occupation',
+  'Description',
+  'Experience',
+  'Skills',
+  'Achievements'
+);
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 import './style.css';
 import './Practice-Components/HOF/higher-order-functions';
+import './Practice-Components/Rest-Parameters/rest-parameters';
