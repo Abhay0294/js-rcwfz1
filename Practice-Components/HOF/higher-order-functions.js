b55f805369c9e10fc5aa23f318d4e60d223c1370
Abhay0294 <abhaypratap349@gmail.com>
2022-08-01 11:03:19 +0530

    1 file modified

--- Practice-Components/HOF/higher-order-functions.js
+++ Practice-Components/HOF/higher-order-functions.js
@@ -1,10 +1,28 @@
+/**
+ * Here callMe is a HOF which when called returns another anonymous function
+ */
 function callMe() {
-  console.log('Thanks for calling!!');
+  return function () {
+    return console.log('Function O/P returned from a HOF');
+  };
 }
 
-let button = document.getElementById('test-button');
-button.addEventListener('click', callMe);
+/**
+ * Below is the first class function which is store in *variable, x
+ */
+let x = () => console.log('Successfully called first class function');
 
+let button = document.getElementById('test-button');
+/*
+ *Here addEventListener() is a HOF which takes another function
+ *as a paramter
+ */
+button.addEventListener('click', x);
+
+/**
+ * Below is the imediately invoked function
+ */
 (() => {
-  return console.log('IIF is called');
+  console.log(callMe()());
+  //return console.log('IIF is called');
 })();
