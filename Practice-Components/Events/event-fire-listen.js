8b3b8f9d3496e30f9c7ad58106e125e79dd8474d
Abhay0294 <abhaypratap349@gmail.com>
2022-08-12 11:34:54 +0530

    3 files modified

--- Practice-Components/Events/event-fire-listen.js
+++ Practice-Components/Events/event-fire-listen.js
@@ -1,12 +1,16 @@
 const button = document.querySelector('#test-button');
+const button2 = document.querySelector('#test-button2');
+const testDiv = document.querySelector('#test-div');
 
-const evt = new CustomEvent('MyEvent', { detail: 'Any Object Here' });
-window.dispatchEvent(evt);
+const myEvent = new CustomEvent('myevent', {
+  detail: {},
+  bubbles: true,
+  cancelable: true,
+  composed: false,
+});
 
-button.addEventListener(
-  'MyEvent',
-  function (evt) {
-    alert(evt.detail);
-  },
-  false
-);
+document.querySelector('#test-button').dispatchEvent(myEvent);
+
+document.querySelector('#test-button').addEventListener('myevent', (event) => {
+  console.log("I'm listening on a custom event");
+});
