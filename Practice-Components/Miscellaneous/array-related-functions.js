4c8c1eff71c6c37f1f4790dde42904bcad9a9a68
Abhay0294 <abhaypratap349@gmail.com>
2022-08-15 16:12:31 +0530

    1 file modified, 1 added

---
+++ Practice-Components/Miscellaneous/array-related-functions.js
@@ -0,0 +1,11 @@
+// const numbers = [1, 2, 3, 4, 1, 3, 4, 5];
+// console.log('Previous Numbers', numbers);
+
+// const filNumbers = [...new Set(numbers)];
+// console.log('Filtered Numbers', filNumbers);
+
+// console.log('###', ...new Set(numbers));
+var arr = [55, 44, 65];
+var set = new Set(arr);
+console.log(set);
+console.log(set.has(65));
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import './style.css';
 //import './Practice-Components/HOF/higher-order-functions';
-//import './Practice-Components/Rest-Parameters/rest-parameters';
+// import './Practice-Components/Rest-Parameters/rest-parameters';
 //import './Practice-Components/Events/prevent-default-functionality';
 // import './Practice-Components/Events/single-event-generatore';
 // import './Practice-Components/Promises/promises';
-import './Practice-Components/Events/event-fire-listen';
+// import './Practice-Components/Events/event-fire-listen';
+import './Practice-Components/Miscellaneous/array-related-functions';
